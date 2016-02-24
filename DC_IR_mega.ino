aa34e514538802101e644a75a245db978daccfd8
guohc <guohc1234@gmail.com>
2016-02-24 12:18:13 -0800

    Added files via upload

---
+++ DC_IR_mega.ino
@@ -0,0 +1,28 @@
+#include <DistanceGP2Y0A21YK.h>
+
+
+DistanceGP2Y0A21YK Dist;
+int distance;
+int proxy=0;
+
+
+void setup() {
+  Serial.begin(9600);
+  Dist.begin(0);
+  
+}
+
+void loop() {
+  distance = Dist.getDistanceCentimeter();  
+  distance = distance*2;
+  Serial.print("\nDistance in centimers: ");
+  Serial.print(distance);
+  if (proxy < distance){
+    proxy = distance;
+  }
+Serial.print ("\nClosest: ");
+Serial.print (proxy)
+  
+
+}
+
